8544df506df3b9185e5581b88d9aaae0401390d4
zoff99 <zoff99@users.noreply.github.com>
2025-11-02 15:24:49 +0100

    dummy to update SORTER file

--- src/main/kotlin/com/zoffcc/applications/undereat/SORTER.kt
+++ src/main/kotlin/com/zoffcc/applications/undereat/SORTER.kt
@@ -1,5 +1,6 @@
 package com.zoffcc.applications.undereat
 
+
 enum class SORTER(val value: Long) {
     NAME(0),
     ADDRESS(1),
@@ -8,3 +9,4 @@ enum class SORTER(val value: Long) {
     ADDED_DATE(4),
     MODIFIED_DATE(5)
 }
+
